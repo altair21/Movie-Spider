0235e3dd35a0bcd82f6233bcafbe60a2144cb0ee
altair21 <lovealtair21@gmail.com>
2018-12-04 16:19:59 +0800

    Improve actor stat

--- tools/genactor.js
+++ tools/genactor.js
@@ -12,17 +12,19 @@ const origin = openFilmOrigin(true).map(obj => ({
   actor: obj.actor ? obj.actor.filter(d => d.id !== '') : [],
 }));
 
+const circleNumber = ['0', '①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨', '⑩', '⑪', '⑫', '⑬', '⑭', '⑮', '⑯', '⑰', '⑱', '⑲', '⑳', '㉑', '㉒', '㉓', '㉔', '㉕', '㉖', '㉗', '㉘', '㉙', '㉚', '㉛', '㉜', '㉝', '㉞', '㉟', '㊱', '㊲', '㊳', '㊴', '㊵', '㊶', '㊷', '㊸', '㊹', '㊺', '㊻', '㊼', '㊽', '㊾', '㊿'];
+
 const getResult = () => {
   const data = [];
 
   origin.forEach((obj) => {
-    const newObj = {
-      name: obj.name,
-      year: obj.year,
-      director: obj.director.join('、'),
-    };
-
-    obj.actor.forEach(act => {
+    obj.actor.forEach((act, index) => {
+      const newObj = {
+        name: obj.name,
+        year: obj.year,
+        director: obj.director.join('、'),
+        actIndex: index + 1,
+      };
       const finded = _.find(data, o => o.id === act.id);
       if (finded) {
         finded.films.push(newObj);
@@ -49,7 +51,7 @@ const getStatisticsText = (res) => {
   res.forEach(obj => {
     logs.push(`${obj.name} 共计 ${obj.films.length} 部影片：`);
     obj.films.forEach(film => {
-      logs.push(`《${film.name}》（${film.director}，${film.year}）`);
+      logs.push(`${circleNumber.length > film.actIndex ? circleNumber[film.actIndex] : film.actIndex}《${film.name}》（${film.director}，${film.year}）`);
     });
     logs.push('');
   });
