8ba2908aadfd354f500fa7878f0d3e8b11c4bc73
altair21 <lovealtair21@gmail.com>
2019-02-28 15:13:34 +0800

    fix recommand tool issue

--- tools/genrecommand.js
+++ tools/genrecommand.js
@@ -4,8 +4,8 @@ import _ from 'lodash';
 
 import { mkdir, openFilmOrigin } from '../src/util/';
 
-const presetWeights = [0, -3, -1, 2, 8, 40];  // 长片分数的影响力
-const shortPresetWeights = [0, -5, -3, 1, 5, 25]; // 短片分数的影响力
+const presetWeights = [1, 1, 0, -3, -1, 2, 8, 40];  // 长片分数的影响力
+const shortPresetWeights = [0, 0, 0, -5, -3, 1, 5, 25]; // 短片分数的影响力
 const punishment = [100, 20, 15, 10, 5, 0]; // 相关影片数量不足时的惩罚力
 const proportion = [0.8, 0.2];  // 新旧计算方式对最终结果的影响力
 
@@ -75,7 +75,7 @@ const getStatisticsText = (res) => {
 };
 
 const findRecommand = () => {
-  const origin = openFilmOrigin(true, o => o.userScore > 0);
+  const origin = openFilmOrigin(true).map(o => ({ ...o, userScore: o.userScore + 2 }));
 
   let films = [];
   let res = [];
